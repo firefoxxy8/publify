4f985cfabb6673a0e63c5cbb9520565099f1364e
Matijs van Zuijlen <Matijs.van.Zuijlen@xs4all.nl>
2008-09-27 22:00:40 +0800

    Simplify and clean up

--- vendor/plugins/local_extensions/lib/local_extensions.rb
+++ vendor/plugins/local_extensions/lib/local_extensions.rb
@@ -1,7 +1,16 @@
+require 'application'
+
 ::ActionController::Base.append_view_path("#{RAILS_ROOT}/vendor/plugins/local_extensions/views")
 
-require 'application'
-module ArticleControllerExtensions
+class LocalController < ContentController
+  layout :theme_layout
+
+  if Blog.default && Blog.default.cache_option == "caches_action_with_params"
+    caches_action_with_params :frontpage
+  else
+    caches_page :frontpage
+  end
+
   def bryarlink
     /id_(\d+)/.match(params[:bryarid])
     begin
@@ -11,8 +20,6 @@ module ArticleControllerExtensions
       return
     rescue ActiveRecord::RecordNotFound
       render :text => "Page not found", :status => 404
-    #rescue
-    #  render :text => "Internal server error", :status => 500
     end
   end
 
@@ -26,24 +33,8 @@ module ArticleControllerExtensions
   end
 end
 
-class LocalController < ContentController
-  include ArticleControllerExtensions
-  layout :theme_layout
-
-  def theme_layout
-    this_blog.current_theme.layout
-  end
-
-  if Blog.default && Blog.default.cache_option == "caches_action_with_params"
-    caches_action_with_params :frontpage
-  else
-    caches_page :frontpage
-  end
-end
-
-module ArticleModelExtensions
+Article.class_eval do
   def pings_closed?
     !(allow_pings? && in_feedback_window?)
   end
 end
-Article.send(:include, ArticleModelExtensions)
