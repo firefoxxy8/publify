a6500cf9ccb1912ff772565af686d418307b2ba9
Matijs van Zuijlen <Matijs.van.Zuijlen@xs4all.nl>
2008-11-04 22:23:56 +0800

    Capistrano configuration

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require 'mongrel_cluster/recipes'
+
+set :application, "typo"
+set :repository,  "."
+
+set :mongrel_conf, "/etc/mongrel-cluster/sites-enabled/#{application}.yml"
+set :mongrel_port, 3100
+set :mongrel_servers, 2
+set :mongrel_prefix, "/blog"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/www.matijs.net/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+set :deploy_via, :copy
+set :copy_strategy, :export
+
+role :app, "mist.matijs.net"
+role :web, "mist.matijs.net"
+role :db,  "mist.matijs.net", :primary => true
+
+after :update_code do
+  sudo "chgrp -R www-data #{current_path}/public"
+end
