155340992f12d355b2da623d4e9a0efa94d3429d
Matijs van Zuijlen <matijs@matijs.net>
2009-02-07 12:58:09 +0100

    Deploy database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,5 +25,36 @@ role :web, "mist.matijs.net"
 role :db,  "mist.matijs.net", :primary => true
 
 after :update_code do
+  # Make public dir accessible to www-data
   sudo "chgrp -R www-data #{current_path}/public"
+  # Link in the production database.yml 
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
+end
+
+namespace :deploy do
+  desc "Create database.yml in shared/config" 
+  task :after_setup do
+    database_configuration = <<-EOF
+development:
+  adapter: sqlite3
+  database: db/#{application}_dev.sqlite3
+
+test:
+  adapter: sqlite3
+  database: db/#{application}_test.sqlite3
+
+production:
+  adapter: postgresql
+  database: #{application}
+  host: 
+  username: #{application}
+  password: 
+EOF
+
+    sudo "mkdir -p #{deploy_to}/#{shared_dir}/config" 
+    sudo "chgrp -R www-data #{deploy_to}/#{shared_dir}/config"
+    sudo "chmod 770 #{deploy_to}/#{shared_dir}/config"
+    put database_configuration, "#{deploy_to}/#{shared_dir}/config/database.yml" 
+    sudo "chmod 750 #{deploy_to}/#{shared_dir}/config"
+  end
 end
