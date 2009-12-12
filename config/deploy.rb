2fed0dff74d86e6755f61083900d158271b88e74
Matijs van Zuijlen <matijs@matijs.net>
2009-12-12 13:12:13 +0100

    Fix new release dir right after upload, before doing migration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,7 @@
+# Magic incantation to give sudo a tty/pty/whatever.
+# See: http://weblog.jamisbuck.org/2007/10/14/capistrano-2-1
+default_run_options[:pty] = true
+
 set :application, "typo"
 set :repository,  "."
 
@@ -12,17 +16,15 @@ role :app, "mist.matijs.net"
 role :web, "mist.matijs.net"
 role :db,  "mist.matijs.net", :primary => true
 
+
 namespace :deploy do
-  desc "After setup: adjust ownership of necessary directories"
-  task :after_setup do
+  # FIXME: Make more descriptive name
+  task :post_setup do
     sudo "chown matijs:matijs #{deploy_to}"
     sudo "chown matijs:matijs #{releases_path}"
     # logdir
     sudo "chgrp -R www-data #{shared_path}/log"
-  end
 
-  desc "Create database.yml in shared/config" 
-  task :after_setup do
     database_configuration = <<-EOF
 development:
   adapter: sqlite3
@@ -47,17 +49,22 @@ EOF
     sudo "chmod 750 #{deploy_to}/#{shared_dir}/config"
   end
 
-  desc "Touchups after symlink"
-  task :after_symlink do
+  task :fix_public_dir do
     # Make public dir accessible to www-data
-    sudo "chgrp -R www-data #{current_path}/public"
-    # Link in the production database.yml 
-    run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{current_path}/config/database.yml" 
+    sudo "chgrp -R www-data #{current_release}/public"
     # Link in the resource folder
-    run "ln -nfs #{shared_path}/files #{current_path}/public/files" 
+    run "ln -nfs #{shared_path}/files #{current_release}/public/files" 
+  end
+
+  task :link_db_config do
+    # Link in the production database.yml 
+    run "ln -nfs #{shared_path}/config/database.yml #{current_release}/config/database.yml" 
   end
 
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+after "deploy:setup", "deploy:post_setup"
+after "deploy:update_code", "deploy:link_db_config", "deploy:fix_public_dir"
