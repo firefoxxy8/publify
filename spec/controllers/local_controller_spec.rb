986b6e971cbc91afb81215daeed62ab4ea150637
Matijs van Zuijlen <matijs@matijs.net>
2011-01-22 18:15:35 +0100

    Update local controller spec to use factory instead of fixture.

--- spec/controllers/local_controller_spec.rb
+++ spec/controllers/local_controller_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe LocalController do
   it 'front page' do
+    Factory(:blog)
     get :frontpage
     response.should render_template(:frontpage)
     assigns[:page_title].should_not be_nil
