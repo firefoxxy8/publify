d9a64c8127d4375c5f81dec4f44c3e6c2cac80ed
Matijs van Zuijlen <matijs@matijs.net>
2011-01-22 23:03:13 +0100

    Remove relative_url_root setting.
    Not needed, and now not working either.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,6 @@ TypoBlog::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  config.action_controller.relative_url_root = "/blog"
-
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
