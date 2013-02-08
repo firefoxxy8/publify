1701b74761748b4726b4e3734d87b33adbe22876
Matijs van Zuijlen <matijs@matijs.net>
2013-02-08 23:56:29 +0100

    Generate nice big cookie secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TypoBlog::Application.config.secret_token = File.open(File.join(Rails.root, "config", "secret.token"), "r") { |f| f.read.delete("\n") }
+TypoBlog::Application.config.secret_token = '53b6d61726851c17c43495b89364179fbf883f49260f8931522e7b470a3651651f07a7f61ff2ab86230a0b63a5f1abda6205925f53804e44a19d6ef3bf775aa4'
