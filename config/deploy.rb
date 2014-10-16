f9e87659fb0a0894207f92a7e73813d7fdf3cfd5
Matijs van Zuijlen <matijs@matijs.net>
2014-10-16 22:30:40 +0200

    Remove comment clutter from Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,38 +3,9 @@ lock '3.2.1'
 
 set :application, 'typo'
 set :repo_url, 'matijs@mist.matijs.net:git/typo.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :branch, 'toxic-elephant'
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, fetch(:linked_dirs, []) + %w{log tmp/pids public/files}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
