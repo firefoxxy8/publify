e6baac08fd8de053e7f7e2c65bfd96f327c8d504
Matijs van Zuijlen <matijs@matijs.net>
2015-01-11 13:05:03 +0100

    Synch capistrano versions in bundle and config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for Capistrano 3.x
+lock '~> 3.2'
 
 set :application, 'typo'
 set :repo_url, 'matijs@mist.matijs.net:git/typo.git'
