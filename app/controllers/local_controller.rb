adde79a5d03829961b67cc0762c21646039e66d7
Matijs van Zuijlen <matijs@matijs.net>
2016-11-27 14:04:07 +0100

    Don't attempt to cache frontpage

--- app/controllers/local_controller.rb
+++ app/controllers/local_controller.rb
@@ -1,8 +1,6 @@
 class LocalController < ContentController
   layout :theme_layout
 
-  caches_page :frontpage
-
   def frontpage
     @frontpage = true
     @articles = Article.already_published.limit(this_blog.limit_article_display)
