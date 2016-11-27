83e735765c51b05530be5681d0cd4ce2642aafa7
Matijs van Zuijlen <matijs@matijs.net>
2016-11-27 14:18:31 +0100

    Update Airbrake configuration

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = '1451e93f0e3b52247c08ed94d1b88f84'
-  config.host    = 'errbit.matijs.net'
-  config.port    = 80
-  config.secure  = config.port == 443
+  config.environment = Rails.env
+  config.ignore_environments = [:test, :development]
+  config.project_key =  '1451e93f0e3b52247c08ed94d1b88f84'
+  config.project_id = 23
 end
